f1ed14d91e18fc6ae5b5186d7e6014bdd0e50d4c
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-06-18 21:40:46 +0700

    Initial Commit

---
+++ QuotesOfTheDay/QuotesOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesOfTheDayApp.swift
+//  QuotesOfTheDay
+//
+//  Created by arifin on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct QuotesOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
