2313ee9f6ca34e564ea0958a03ca369ba936d12a
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-07-06 19:36:34 +0700

    Refactor, cleaning up

--- QuotesOfTheDay/NotificationManager.swift
+++ QuotesOfTheDay/NotificationManager.swift
@@ -7,38 +7,61 @@ protocol NotificationManager {
 final class LocalNotificationManager: NotificationManager {
     
     func scheduleDailyNotifications() {
-        let center = UNUserNotificationCenter.current()
-        center.removeAllPendingNotificationRequests() // Remove previous notifications if any
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.removeAllPendingNotificationRequests()
+        notificationCenter.add(morningNotificationRequest())
+        notificationCenter.add(eveningNotificationRequest())
+    }
+    
+    private func morningNotificationRequest() -> UNNotificationRequest {
+        // Schedule morning notification at 7 AM
+        var dateComponents = DateComponents()
+        dateComponents.hour = 7
+        dateComponents.minute = 0
+        dateComponents.timeZone = .current
+        let morningTrigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents,
+            repeats: true
+        )
 
+        return UNNotificationRequest(
+            identifier: "morningNotification",
+            content: morningContent(),
+            trigger: morningTrigger
+        )
+    }
+    
+    private func morningContent() -> UNMutableNotificationContent {
         let morningContent = UNMutableNotificationContent()
         morningContent.title = "Good Morning!"
         morningContent.body = "Start your day with a motivational quote. See quote here."
         morningContent.sound = .default
+        return morningContent
+    }
+    
+    private func eveningNotificationRequest() -> UNNotificationRequest {
+        // Schedule evening notification at 8 PM
+        var dateComponents = DateComponents()
+        dateComponents.hour = 19
+        dateComponents.minute = 27
+        dateComponents.timeZone = .current
+        let eveningTrigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents,
+            repeats: true
+        )
 
+        return UNNotificationRequest(
+            identifier: "eveningNotification",
+            content: eveningContent(),
+            trigger: eveningTrigger
+        )
+    }
+    
+    private func eveningContent() -> UNMutableNotificationContent {
         let eveningContent = UNMutableNotificationContent()
         eveningContent.title = "Good Evening!"
         eveningContent.body = "End your day with a motivational quote. See quote here!"
         eveningContent.sound = .default
-
-        // Schedule morning notification at 7 AM
-        var morningDateComponents = DateComponents()
-        morningDateComponents.hour = 7
-        morningDateComponents.minute = 0
-        morningDateComponents.timeZone = .current
-        let morningTrigger = UNCalendarNotificationTrigger(dateMatching: morningDateComponents, repeats: true)
-
-        let morningRequest = UNNotificationRequest(identifier: "morningNotification", content: morningContent, trigger: morningTrigger)
-        center.add(morningRequest)
-
-        // Schedule evening notification at 8 PM
-        var eveningDateComponents = DateComponents()
-        eveningDateComponents.hour = 20
-        eveningDateComponents.minute = 0
-        morningDateComponents.timeZone = .current
-        let eveningTrigger = UNCalendarNotificationTrigger(dateMatching: eveningDateComponents, repeats: true)
-
-        let eveningRequest = UNNotificationRequest(identifier: "eveningNotification", content: eveningContent, trigger: eveningTrigger)
-        center.add(eveningRequest)
+        return eveningContent
     }
-
 }
