cb265a060725481857a3d1b047fbc324772d1016
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-07-06 19:21:47 +0700

    Add notification funtionality

--- QuotesOfTheDay/ContentView.swift
+++ QuotesOfTheDay/ContentView.swift
@@ -56,6 +56,9 @@ struct ContentView: View {
             .tint(.brown)
         }
         .padding()
+        .onAppear {
+            scheduleDailyNotifications()
+        }
     }
 }
 
@@ -66,4 +69,39 @@ struct ContentView: View {
 #Preview {
     ContentView(viewModel: ContentViewModel(quoteGenerator: RandomQuoteGenerator()))
         .preferredColorScheme(.dark)
+}
+
+func scheduleDailyNotifications() {
+    let center = UNUserNotificationCenter.current()
+    center.removeAllPendingNotificationRequests() // Remove previous notifications if any
+
+    let morningContent = UNMutableNotificationContent()
+    morningContent.title = "Good Morning!"
+    morningContent.body = "Start your day with a motivational quote. See quote here."
+    morningContent.sound = .default
+
+    let eveningContent = UNMutableNotificationContent()
+    eveningContent.title = "Good Evening!"
+    eveningContent.body = "End your day with a motivational quote. See quote here!"
+    eveningContent.sound = .default
+
+    // Schedule morning notification at 7 AM
+    var morningDateComponents = DateComponents()
+    morningDateComponents.hour = 7
+    morningDateComponents.minute = 0
+    morningDateComponents.timeZone = .current
+    let morningTrigger = UNCalendarNotificationTrigger(dateMatching: morningDateComponents, repeats: true)
+
+    let morningRequest = UNNotificationRequest(identifier: "morningNotification", content: morningContent, trigger: morningTrigger)
+    center.add(morningRequest)
+
+    // Schedule evening notification at 8 PM
+    var eveningDateComponents = DateComponents()
+    eveningDateComponents.hour = 20
+    eveningDateComponents.minute = 0
+    morningDateComponents.timeZone = .current
+    let eveningTrigger = UNCalendarNotificationTrigger(dateMatching: eveningDateComponents, repeats: true)
+
+    let eveningRequest = UNNotificationRequest(identifier: "eveningNotification", content: eveningContent, trigger: eveningTrigger)
+    center.add(eveningRequest)
 }
--- QuotesOfTheDay/QuotesOfTheDayApp.swift
+++ QuotesOfTheDay/QuotesOfTheDayApp.swift
@@ -10,9 +10,29 @@ import Quote
 
 @main
 struct QuotesOfTheDayApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: ContentViewModel(quoteGenerator: RandomQuoteGenerator()))
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        requestNotificationAuthorization()
+        return true
+    }
+
+    private func requestNotificationAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Authorization request error: \(error.localizedDescription)")
+            }
         }
     }
 }
